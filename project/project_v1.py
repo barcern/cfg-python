f46bc1a7a1d28ca787044a0be8f6d79f5ea45e7b
barcern <bcernakova01@gmail.com>
2020-10-29 16:14:45 +0000

    Update project_v1.py

--- project/project_v1.py
+++ project/project_v1.py
@@ -18,20 +18,27 @@ print(len(data))
 
 
 print((data[0]))
+
+
 current_data = data
+dog_list = []
+dog_list_old = []
 flag = True
 categories = ['weight', 'height', 'breed_group', 'temperament']
 print("Welcome to my dog breed recommendation app!")
 while flag:
+    
     # User input to choose a category
     user_category = input("Which category would you like to consider? "
                           "weight / height / breed_group / "
                           "temperament ").lower()
+    
     # Check whether user input is recognised
     if user_category in categories:
         pass
     else:
         print("I'm sorry but we don't recognise this category.")
+        
     # User input for temperament
     user_selection = input(f"What {user_category} would you like your "
                            "dog to be? ")
@@ -43,7 +50,11 @@ while flag:
         else:
             if dog[user_category].find(user_selection) != -1:
                 print(dog['name'])
-    user_continue = input("Would you like to continue with your serach? y/n ")
+                dog_list.append(dog['name'])
+    user_continue = input("Would you like to continue with your search? y/n ")
     if user_continue == "n":
         flag = False
+print(dog_list)
+
+print(data)
         
